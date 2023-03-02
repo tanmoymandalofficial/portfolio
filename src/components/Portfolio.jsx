ceaa8afd7a5102e9a309c92321425eadc1aff65a
tanmoymandalofficial <tm.socialmidea@gmail.com>
2023-03-02 13:21:07 +0530

    update

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -64,7 +64,7 @@ const Portfolio = () => {
 
   return (
     <div
-      name="portfolio"
+      name="Projects"
       className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-fit"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-fit">
